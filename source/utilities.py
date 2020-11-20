9798f759085e1ca5f72a8935584cfcc82b1f1c6d
NishanthJKumar <nishanth.kumar20@gmail.com>
2020-11-19 21:54:09 -0500

    added rotation utility!

--- source/utilities.py
+++ source/utilities.py
@@ -20,3 +20,39 @@ def is_image(image: np.ndarray) -> bool:
         and image.dtype == np.uint8
         and image.shape[2] == 3
     )
+
+
+def convert_angles_to_matrix(
+    x_angle: float, y_angle: float, z_angle: float
+) -> np.ndarray:
+    # Takes angles in degrees of a rotation about x, y and z axes respectively
+    # and returns a matrix!
+    x_angle = np.deg2rad(x_angle)
+    y_angle = np.deg2rad(y_angle)
+    z_angle = np.deg2rad(z_angle)
+
+    x_matrix = np.array(
+        [
+            [1.0, 0.0, 0.0],
+            [0.0, np.cos(x_angle), -np.sin(x_angle)],
+            [0.0, np.sin(x_angle), np.cos(x_angle)],
+        ]
+    )
+
+    y_matrix = np.array(
+        [
+            [np.cos(y_angle), 0.0, np.sin(y_angle)],
+            [0.0, 1.0, 0.0],
+            [-np.sin(y_angle), 0.0, np.cos(y_angle)],
+        ]
+    )
+
+    z_matrix = np.array(
+        [
+            [np.cos(z_angle), -np.sin(z_angle), 0.0],
+            [np.sin(z_angle), np.cos(z_angle), 0.0],
+            [0.0, 0.0, 1.0],
+        ]
+    )
+
+    return np.matmul(z_matrix, np.matmul(y_matrix, x_matrix))
